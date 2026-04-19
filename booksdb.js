5b8796bbb5cc800084514ade71629ea5940e6833
hossam ahmed <hossamahmed5002@gmail.com>
2026-04-20 00:32:21 +0200

    Initial commit - Book Review Application

---
+++ booksdb.js
@@ -0,0 +1,24 @@
+let books = {
+  1: { author: "Chinua Achebe", title: "Things Fall Apart", reviews: {} },
+  2: { author: "Hans Christian Andersen", title: "Fairy tales", reviews: {} },
+  3: { author: "Dante Alighieri", title: "The Divine Comedy", reviews: {} },
+  4: { author: "Unknown", title: "The Epic Of Gilgamesh", reviews: {} },
+  5: { author: "Unknown", title: "The Book Of Job", reviews: {} },
+  6: { author: "Unknown", title: "One Thousand and One Nights", reviews: {} },
+  7: { author: "Unknown", title: "Nj\u00e1ls Saga", reviews: {} },
+  8: { author: "Jane Austen", title: "Pride and Prejudice", reviews: {} },
+  9: { author: "Honor\u00e9 de Balzac", title: "Le P\u00e8re Goriot", reviews: {} },
+  10: { author: "Samuel Beckett", title: "Molloy, Malone Dies, The Unnamable, the trilogy", reviews: {} },
+  11: { author: "Giovanni Boccaccio", title: "The Decameron", reviews: {} },
+  12: { author: "Jorge Luis Borges", title: "Ficciones", reviews: {} },
+  13: { author: "Emily Bront\u00eb", title: "Wuthering Heights", reviews: {} },
+  14: { author: "Albert Camus", title: "The Stranger", reviews: {} },
+  15: { author: "Paul Celan", title: "Poems", reviews: {} },
+  16: { author: "Luis de Cervantes", title: "Don Quixote", reviews: {} },
+  17: { author: "Anton Chekhov", title: "Stories", reviews: {} },
+  18: { author: "Joseph Conrad", title: "Nostromo", reviews: {} },
+  19: { author: "Charles Dickens", title: "Great Expectations", reviews: {} },
+  20: { author: "Denis Diderot", title: "Jacques the Fatalist", reviews: {} }
+};
+
+module.exports = books;
